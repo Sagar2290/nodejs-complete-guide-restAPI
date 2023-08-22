41a74c31ab66aeea67414f92db209ff605b48d6a
Sagar <bhagwatsagar2290@gmail.com>
2023-08-22 17:03:21 +0530

    Added Authentication & Authorization

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 const { v4: uuidv4 } = require("uuid");
 
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -15,7 +16,7 @@ const fileStorage = multer.diskStorage({
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, uuidv4() + '-' + file.originalname);
+    cb(null, uuidv4() + "-" + file.originalname);
   },
 });
 
@@ -49,11 +50,14 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
+app.use("/auth", authRoutes);
+
 app.use((error, req, res, next) => {
   console.log(error);
   const status = error.statusCode || 500;
   const message = error.message;
-  res.status(status).json({ message: message });
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
 mongoose
